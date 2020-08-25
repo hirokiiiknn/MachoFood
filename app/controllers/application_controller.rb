39b7afd6d5cec0249b1365d89a700da4cf8bfc4b
hirokiiiknn <62924651+hirokiiiknn@users.noreply.github.com>
2020-08-25 16:10:33 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,10 @@
 class ApplicationController < ActionController::Base
-  class MyLogger < Logger
-    include ActiveSupport::LoggerSilence
-    # include LoggerSilence
-    include ActiveSupport::LoggerThreadSafeLevel
-  end
-  
-
-  before_action :mylogger_test
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :image])
-  end
-
-  def mylogger_test
-    mylogger = MyLogger.new(STDOUT)
-    mylogger.silence do
-      mylogger.debug("controller = #{controller_name}")
-      mylogger.info("action = #{action_name}")
-      mylogger.error("controler#action = #{controller_name}##{action_name}")
-    end
   end
 end
 
