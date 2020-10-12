c9a5011f8f6ce305383c511941ca86456f57c6b9
hirokiiiknn <62924651+hirokiiiknn@users.noreply.github.com>
2020-10-12 16:26:47 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   root to: 'tweets#index'
   devise_for :users,
   controllers: {
-    sessions: 'users/sessions',
-    registrations: "users/registrations",
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
 
