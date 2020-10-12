873a6a8a521ea90048bcf248bcb47ed3d9026974
hirokiiiknn <62924651+hirokiiiknn@users.noreply.github.com>
2020-10-12 17:33:07 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_10_063502) do
+ActiveRecord::Schema.define(version: 2020_10_12_082836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 2020_10_10_063502) do
     t.string "instagram"
     t.string "facebook"
     t.string "twitter"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
