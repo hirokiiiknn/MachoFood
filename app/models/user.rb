417746dbc2b294d8d558881866ce705be4d59619
hirokiiiknn <62924651+hirokiiiknn@users.noreply.github.com>
2020-10-14 18:03:46 +0900

    l

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,52 +30,6 @@ class User < ApplicationRecord
   end
 
   # コールバック関数（facebook, googleのsns認証）
-  # def self.without_sns_data(auth)
-  #   user = User.where(email: auth.info.email).first
-
-  #     if user.present?
-  #       sns = SnsCredential.create(
-  #         uid: auth.uid,
-  #         provider: auth.provider,
-  #         user_id: user.id
-  #       )
-  #     else
-  #       user = User.new(
-  #         nickname: auth.info.name,
-  #         email: auth.info.email,
-  #       )
-  #       sns = SnsCredential.new(
-  #         uid: auth.uid,
-  #         provider: auth.provider
-  #       )
-  #     end
-  #     return { user: user ,sns: sns}
-  #   end
-
-  #  def self.with_sns_data(auth, snscredential)
-  #   user = User.where(id: snscredential.user_id).first
-  #   unless user.present?
-  #     user = User.new(
-  #       nickname: auth.info.name,
-  #       email: auth.info.email,
-  #     )
-  #   end
-  #   return {user: user}
-  #  end
-
-  #  def self.find_oauth(auth)
-  #   uid = auth.uid
-  #   provider = auth.provider
-  #   snscredential = SnsCredential.where(uid: uid, provider: provider).first
-  #   if snscredential.present?
-  #     user = with_sns_data(auth, snscredential)[:user]
-  #     sns = snscredential
-  #   else
-  #     user = without_sns_data(auth)[:user]
-  #     sns = without_sns_data(auth)[:sns]
-  #   end
-  #   return { user: user ,sns: sns}
-  # end
   def self.find_or_create_from_auth(auth)
     find_or_create_by(email: auth.info.email) do |user|
       user.password = Devise.friendly_token[0, 20]
